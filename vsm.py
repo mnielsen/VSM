6042bc6cae5090371b5166968adfe248266ff310
Michael Nielsen <mn@michaelnielsen.org>
2011-07-07 14:59:48 -0400

    Refactoring.

--- vsm.py
+++ vsm.py
@@ -10,8 +10,8 @@ import math
 import sys
 
 # We use a corpus of four documents.  Each document has an id, and
-# these are the keys in the following dict, together with the
-# corresponding filename.
+# these are the keys in the following dict.  The values are the
+# corresponding filenames.
 document_filenames = {0 : "documents/lotr.txt",
                       1 : "documents/silmarillion.txt",
                       2 : "documents/rainbows_end.txt",
@@ -20,22 +20,31 @@ document_filenames = {0 : "documents/lotr.txt",
 # The size of the corpus
 N = len(document_filenames)
 
-# dictionary: all terms (i.e., words) in the document corpus
+# dictionary: a set to contain all terms (i.e., words) in the document
+# corpus.
 dictionary = set()
 
-# postings: key: term; value: a dict whose keys are document ids, and
-# whose values are the frequency with which the term occurs in the
-# document.
+# postings: a defaultdict whose keys are terms, and whose
+# corresponding values are the so-called "postings list" for that
+# term, i.e., the list of documents the term appears in.
 #
-# Note that it is conventional to call postings[term] the postings
-# list for term.
+# The way we implement the postings list is actually not as a Python
+# list.  Rather, it's as a dict whose keys are the document ids of
+# documents that the term appears in, with corresponding values equal
+# to the frequency with which the term occurs in the document.
+#
+# As a result, postings[term] is the postings list for term, and
+# postings[term][id] is the frequency with which term appears in
+# document id.
 postings = defaultdict(dict)
 
-# document_frequency: key: term;  value: how many docs contain the key
+# document_frequency: a defaultdict whose keys are terms, with
+# corresponding values equal to the number of documents which contain
+# the key, i.e., the document frequency.
 document_frequency = defaultdict(int)
 
-# length: key: document id; value: the Euclidean length of the
-# corresponding document vector.
+# length: a defaultdict whose keys are document ids, with values equal
+# to the Euclidean length of the corresponding document vector.
 length = defaultdict(float)
 
 # The list of characters (mostly, punctuation) we want to strip out of
@@ -43,7 +52,7 @@ length = defaultdict(float)
 characters = " .,!#$%^&*();:\n\t\\\"?!{}[]<>"
 
 def main():
-    initalize_terms_and_postings()
+    initialize_terms_and_postings()
     initialize_document_frequencies()
     initialize_lengths()
     while True:
@@ -64,7 +73,7 @@ def initialize_terms_and_postings():
         unique_terms = set(terms)
         dictionary = dictionary.union(unique_terms)
         for term in unique_terms:
-            postings[term][id] = terms.count(term) # value is the
+            postings[term][id] = terms.count(term) # the value is the
                                                    # frequency of the
                                                    # term in the
                                                    # document
@@ -72,7 +81,8 @@ def initialize_terms_and_postings():
 def tokenize(document):
     """Returns a list whose elements are the separate terms in
     document.  Something of a hack, but for the simple documents we're
-    using, it's okay."""
+    using, it's okay.  Note that we case-fold when we tokenize, i.e.,
+    we lowercase everything."""
     terms = document.lower().split()
     return [term.strip(characters) for term in terms]
 
@@ -113,7 +123,10 @@ def do_search():
     list of relevant documents, in decreasing order of cosine
     similarity."""
     query = tokenize(raw_input("Search query >> "))
-    # find document ids containing all query terms
+    if query == []:
+        sys.exit()
+    # find document ids containing all query terms.  Works by
+    # intersecting the posting lists for all query terms.
     relevant_document_ids = intersection(
             [set(postings[term].keys()) for term in query])
     if not relevant_document_ids:
@@ -123,20 +136,21 @@ def do_search():
                          for id in relevant_document_ids],
                         key=lambda x: x[1],
                         reverse=True)
+        print "Score: filename"
         for (id,score) in scores:
             print str(score)+": "+document_filenames[id]
 
 def intersection(sets):
     """Returns the intersection of all sets in the list sets. Requires
-    that sets contains at least one element, otherwise it raises an
-    error."""
+    that the list sets contains at least one element, otherwise it
+    raises an error."""
     return reduce(set.intersection, [s for s in sets])
 
 def similarity(query,id):
     """Returns the cosine similarity between query and document id.
-    Note that this is unnormalized by the length of the query vector,
-    since this doesn't make any difference to the ordering of
-    results."""
+    Note that we don't bother dividing by the length of the query
+    vector, since this doesn't make any difference to the ordering of
+    search results."""
     similarity = 0.0
     for term in query:
         if term in dictionary:
